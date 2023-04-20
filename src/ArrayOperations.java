48adbeadef46e1139e2d801e0ac3b47ebe289b2d
EgorBusuioc <busuiocegor@gmail.com>
2023-04-20 15:02:29 +0300

    Code documentation

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -1,16 +1,34 @@
 import java.util.Random;
 
+/**
+ * <h3>This class provides access to arrays in three different ways:</h3>
+ * <p>1) direct access to vector elements.</p>
+ * <p>2) access via Iliff vector.</p>
+ * <p>3) the method of defining vectors.</p>
+ * @see ArrayOperations#getElement(int, int)
+ * @see ArrayOperations#getHilbertElement(int)
+ * @see ArrayOperations#getColumnVector(int)
+ *
+ *
+ * @version 1.0
+ * @author EgorBusuioc
+ */
 public class ArrayOperations {
 
     private int[][] array;
     private int numRows;
     private int numCols;
 
+    /**
+     * <h3>Constructor with parameters of the <b>ArrayOperations</b> class for creating and filling an array with elements from -100 to 100</h3>
+     *
+     * @param numRows number of rows.
+     * @param numCols number of columns.
+     */
     public ArrayOperations(int numRows, int numCols) {
         array = new int[numRows][numCols];
         this.numRows = numRows;
         this.numCols = numCols;
-        // заполнение массива случайными значениями от 0 до 1
         Random random = new Random();
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
@@ -19,12 +37,21 @@ public class ArrayOperations {
         }
     }
 
-    // прямой доступ к элементам массива
+    /**
+     * <h3>Direct access to vector elements</h3>
+     * @param row number of rows
+     * @param col number of columns
+     * @return Array value at the position [row][column]
+     */
     public int getElement(int row, int col) {
         return array[row][col];
     }
 
-    // доступ посредством векторов Айлиффа
+    /**
+     * <h3>Access via Iliff vector.</h3>
+     * @param n number of element, <b>starts from 1, not from 0</b>
+     * @return Array value at the position [row][column]
+     */
     public int getHilbertElement(int n) {
         int row = n - 1;
         int col = 0;
@@ -35,7 +62,11 @@ public class ArrayOperations {
         return array[row][col];
     }
 
-    // метод определяющий векторы
+    /**
+     * <h3>The method of defining vectors</h3>
+     * @param col column number
+     * @return Vector
+     */
     public int[] getColumnVector(int col) {
         int[] vector = new int[numRows];
         for (int i = 0; i < numRows; i++) {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
+/**
+ * <h3>This class is needed to call methods of the class</h3>
+ * @see ArrayOperations
+ *
+ * @version 1.0
+ * @author EgorBusuioc
+ */
 public class Main {
     public static void main(String[] args) {
 
@@ -7,7 +14,7 @@ public class Main {
         int[] colVec = array.getColumnVector(15);
 
         System.out.println("Direct access to vector elements: " + array.getElement(15, 17));
-        System.out.println("Access via Ayliffe vectors: " + array.getHilbertElement(15));
+        System.out.println("Access via Iliff vectors: " + array.getHilbertElement(15));
         System.out.println("Get column Vector: " + Arrays.toString(colVec));
     }
 }
